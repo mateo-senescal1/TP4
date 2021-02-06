7c048b313355d1052249c2e83c46a1fbf2191f29
Petru Valicov <petru.valicov@etu.umontpellier.fr>
2021-02-06 17:31:32 +0100

    classe de test pour produit

--- src/test/java/fr/umontpellier/iut/ProduitTest.java
+++ src/test/java/fr/umontpellier/iut/ProduitTest.java
@@ -7,21 +7,57 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProduitTest {
+
     private Produit produit;
     private Compte compte;
+    private OffreEnchere o;
 
     @BeforeEach
     public void initialisation() {
         produit = new Produit(0, "", 0, 152);
-        compte = new Compte("toto", "lolo@fifi.com", "1 av. champs elysees",59);
+        compte = new Compte("toto", "lolo@fifi.com", "1 av. champs elysees", 59);
+        o = new OffreEnchere(15, 20, produit, compte);
+        produit.setPasEnchere(2);
+        produit.demarrerEnchere();
+    }
+
+    @Disabled
+    @Test
+    void test_verifier_offre_enchere_non_demarree(){
+        Produit p = new Produit(0, "", 0, 152);
+        assertFalse(p.verifierOffre(o));
+    }
+
+    @Disabled
+    @Test
+    void test_verifier_offre_quand_pas_d_offre_et_pas_enchere_non_respecte(){
+        assertTrue(produit.verifierOffre(o));
+    }
+
+    @Disabled
+    @Test
+    void test_verifier_offre_quand_il_y_a_offre_et_pas_enchere_non_respecte(){
+        produit.ajouterOffre(o);
+        assertFalse(produit.verifierOffre(new OffreEnchere(16,900, produit,compte)));
     }
 
     @Disabled
     @Test
     void ajouterOffre_quand_pas_d_offre() {
-        OffreEnchere o = new OffreEnchere(15, 20, produit,compte);
+        OffreEnchere o = new OffreEnchere(15, 20, produit, compte);
         produit.ajouterOffre(o);
         assertSame(produit.getGagnant(), o);
     }
+
+    @Disabled
+    @Test
+    public void ajouterOffre_quand_il_y_a_offre_pas_enchere_non_respecte() {
+        OffreEnchere o = new OffreEnchere(15, 20, produit, compte);
+        produit.ajouterOffre(o);
+        OffreEnchere o2 = new OffreEnchere(16, 20, produit, compte);
+
+        assertSame(produit.getGagnant(), o);
+    }
+
 
 }
